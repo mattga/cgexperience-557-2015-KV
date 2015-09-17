722d88dc5cd765f020eeee4f38d658a31e765e4b
Kyle Vansice <vansicek@gmail.com>
2015-09-16 23:31:51 -0500

    no message

--- Homework_2p2/main.cpp
+++ Homework_2p2/main.cpp
@@ -2,8 +2,7 @@
 //  main.cpp
 //  OpenGL4Test
 //
-//  Created by Rafael Radkowski on 5/28/15.
-//  Copyright (c) 2015 -. All rights reserved.
+//  Created by Kyle Vansice on 9/14/15.
 //
 
 // stl include
